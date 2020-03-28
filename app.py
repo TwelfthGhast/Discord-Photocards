4416f45e9fc1861d9209d2819c711feb15013b37
TwelfthGhast <dennisyingswebpages@gmail.com>
2020-03-28 11:30:20 +1100

    Basic saving of users + admin perms

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
-from functions import cmd_unlock, cmd_tcg, cmd_print_collections
+from functions import cmd_unlock, cmd_tcg, cmd_print_collections, cmd_lock
 from util import TCGCollection, TCGUser, getTCGUser, getCollection, ImageToByteStream
 from PIL import Image, ImageDraw
 import discord
 import math
-
+import signal
+import pickle
+import atexit
+import threading
+from time import sleep
+import datetime
+token = ""
 
 
 client = discord.Client()
@@ -13,7 +19,47 @@ COMMAND_ONE = '$tcg'
 COMMAND_TWO = '$unlock'
 COMMAND_THREE = '$collections'
 COMMAND_FOUR = '$help'
-ALL_COMMANDS = [COMMAND_ONE, COMMAND_TWO, COMMAND_THREE, COMMAND_FOUR]
+COMMAND_FIVE = '$lock'
+COMMON_CMD = [COMMAND_ONE, COMMAND_THREE, COMMAND_FOUR]
+ADMIN_CMD = [COMMAND_TWO, COMMAND_FIVE]
+
+ADMINS = [
+    "12Ghast#4326"
+]
+
+BACKUP_DIR = "backup"
+USER_BACKUP = f"{BACKUP_DIR}/user.p"
+
+
+def cleanup_function():
+    print("Attempting to save users...")
+    try:
+        pickle_user_file = open(USER_BACKUP, "wb")
+        pickle.dump(TCGUser.all_users, pickle_user_file)
+        pickle_user_file.close()
+        print("Saved users")
+    except Exception as err:
+        print("An error occurred when saving user database:")
+        print(e)
+
+
+def regular_backup(seconds=60*60*3):
+    while True:
+        sleep(seconds)
+        time = datetime.datetime.now()
+        backup_name = f"{BACKUP_DIR}/user-{int(time.timestamp())}.p"
+        print(
+            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Saving a backup to {backup_name}")
+        pickle_user_file = open(backup_name, "wb")
+        pickle.dump(TCGUser.all_users, pickle_user_file)
+        pickle_user_file.close()
+
+
+def read_pickle(filename):
+    pickle_msg_file = open(filename, "rb")
+    data = pickle.load(pickle_msg_file)
+    pickle_msg_file.close()
+    return data
 
 
 @client.event
@@ -25,44 +71,53 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if str(message.channel) != "bot-spam":
+    elif str(message.channel) != "vcard-game-testing":
         return
+
+    args = message.content.split()
+    if args[0] in ADMIN_CMD:
+        if str(message.author) not in ADMINS:
+            await message.channel.send("You do not have permission to perform this action!")
+            return
 
     # General TCG menu
     if message.content.startswith(COMMAND_ONE):
         await cmd_tcg(message, COMMAND_ONE)
-
     # Unlock cards in collection
-    if message.content.startswith(COMMAND_TWO):
+    elif message.content.startswith(COMMAND_TWO):
         await cmd_unlock(message, COMMAND_TWO)
-
     # List all collections
-    if message.content.startswith(COMMAND_THREE):
+    elif message.content.startswith(COMMAND_THREE):
         await cmd_print_collections(message, COMMAND_THREE)
-
     # List all commands
-    if message.content.startswith(COMMAND_FOUR):
+    elif message.content.startswith(COMMAND_FOUR):
         msg_list = ["Here are all the commands:"]
+        ALL_COMMANDS = COMMON_CMD
+        if str(message.author) in ADMINS:
+            ALL_COMMANDS += ADMIN_CMD
         for command in ALL_COMMANDS:
             msg_list.append(f"\t\t- {command}")
         msg_collected = "\n".join(msg_list)
         await message.channel.send(msg_collected)
-        return
+    # Lock cards in collection
+    elif message.content.startswith(COMMAND_FIVE):
+        await cmd_lock(message, COMMAND_FIVE)
+    return
 
 for collection in os.listdir("collections"):
     width = 0
     height = 0
     images = []
     preview = None
-    for image in os.listdir(f"collections/{collection}"):
+    for image in sorted(os.listdir(f"collections/{collection}")):
         if image == "dimensions.txt":
             fh = open(f"collections/{collection}/{image}", "r")
             try:
                 width = int(fh.readline())
                 height = int(fh.readline())
             except Exception as e:
-                print(f"Error reading dimensions.txt from collections/{collection}:\t{e}")
+                print(
+                    f"Error reading dimensions.txt from collections/{collection}:\t{e}")
                 print(f"Skipping collection...")
                 break
         elif image == "preview.jpg":
@@ -71,6 +126,23 @@ for collection in os.listdir("collections"):
             images.append(image)
     if width == 0 or height == 0 or len(images) == 0:
         next
-    temp_TCG = TCGCollection(collection, images, (width, height), math.ceil(math.sqrt(len(images))), preview)
+    temp_TCG = TCGCollection(collection, images, (width, height), math.ceil(
+        math.sqrt(len(images))), preview)
+
+print("Creating thread for periodic backups")
+x = threading.Thread(target=regular_backup, daemon=True)
+x.start()
+
+print("Attempting to import users from file")
+if os.path.exists(USER_BACKUP) and os.path.isfile(USER_BACKUP):
+    try:
+        TCGUser.all_users = read_pickle(USER_BACKUP)
+        print(f"Imported users from {USER_BACKUP}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+else:
+    print(f"Could not find {USER_BACKUP}")
+atexit.register(cleanup_function)
 
 client.run(token)
--- functions.py
+++ functions.py
@@ -84,7 +84,7 @@ async def cmd_tcg(message, COMMAND_NAME):
             await message.channel.send(f"You do not own card number {item} in '{collClass.name}'!")
             return
         else:
-            imgs = [discord.File(collClass.items[item - 1],
+            imgs = [discord.File(f"collections/{collClass.name}/{collClass.items[item-1]}",
                                  filename="collection.jpeg")]
             await message.channel.send(f"Card {item} in '{collClass.name}'", files=imgs)
             return
@@ -145,7 +145,52 @@ async def cmd_unlock(message, COMMAND_NAME):
     else:
         await message.channel.send(f"{COMMAND_NAME} help\n\nUsage:\n\t\t{COMMAND_NAME} <collection> <n> <username>\
                 \n\t\t\t\t - Unlocks the <n>th item in <collection> for <username>\
-                \n\t\t\t\t - <username> should be in format NAME#0000")
+                \n\t\t\t\t - <username> should be a discord mention")
+    return
+
+async def cmd_lock(message, COMMAND_NAME):
+    inputdata = message.content.split()
+    len_input = len(inputdata)
+    if len_input == 4:
+        cmd, coll, item, u_mention = inputdata
+        collClass = getCollection(coll)
+        if not collClass:
+            await message.channel.send(f"The collection '{coll}' does not exist!")
+            return
+        try:
+            item = int(item)
+        except:
+            await message.channel.send(f"'{item}' is not a valid item number!")
+            return
+        if item > collClass.size:
+            await message.channel.send(f"The collection '{coll}' has {collClass.size} items, but you wanted number {item}!")
+            return
+        if item < 0:
+            await message.channel.send(f"'{item}' is not a valid item number!")
+            return
+        try:
+            user_mention = message.mentions[0]
+        except:
+            await message.channel.send(f"You did not mention anyone in the command!")
+            return
+        for g_member in message.channel.guild.members:
+            if g_member == user_mention:
+                u_name = str(g_member)
+                uClass = getTCGUser(u_name)
+                if collClass.name in uClass.collected:
+                    if item not in uClass.collected[collClass.name]:
+                        await message.channel.send(f"{u_name} does not own card {item} of collection '{collClass.name}'!")
+                        return
+                    uClass.collected[collClass.name].remove(item)
+                else:
+                    uClass.collected[collClass.name] = []
+                await message.channel.send(f"{message.author.mention} removed card number {item} from '{collClass.name}' from  {user_mention.mention} :(")
+                return
+        await message.channel.send(f"{u_mention} was not found in the server.")
+    else:
+        await message.channel.send(f"{COMMAND_NAME} help\n\nUsage:\n\t\t{COMMAND_NAME} <collection> <n> <username>\
+                \n\t\t\t\t - Locks the <n>th item in <collection> for <username>\
+                \n\t\t\t\t - <username> should be a discord mention")
     return
 
 
