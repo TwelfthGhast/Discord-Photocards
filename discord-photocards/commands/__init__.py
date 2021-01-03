c6e1b349a6edee53a201d4103fab9f5cfee83ad4
Dennis Ying <dennisyingswebpages@gmail.com>
2021-01-03 04:27:50 +0000

    Change image return order + add debug command

--- discord-photocards/commands/__init__.py
+++ discord-photocards/commands/__init__.py
@@ -1,4 +1,5 @@
 from .base_class import get_command_class, BotCommand
+from .debug import DebugBotCommand
 from .view import ViewBotCommand
 from .help import HelpBotCommand
 from .unlock import UnlockBotCommand
