367c19923976f8ab12caacd4cf1ed6be7dadae93
Dennis Ying <dennisyingswebpages@gmail.com>
2021-01-03 03:09:04 +0000

    Locking also works

--- discord-photocards/commands/__init__.py
+++ discord-photocards/commands/__init__.py
@@ -2,3 +2,4 @@ from .base_class import get_command_class, BotCommand
 from .view import ViewBotCommand
 from .help import HelpBotCommand
 from .unlock import UnlockBotCommand
+from .lock import LockBotCommand
